6d3014b9d24e04f5ce87f250a0ed3801f560a115
Juan <jfmartinez2598@gmail.com>
2022-06-30 11:31:57 -0300

    Primer commit

---
+++ src/helpers/getGif.js
@@ -0,0 +1,13 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=4SsH3khmVM5C5vD0douPgB2VrnCq6yBx&q=${category}&limit=10`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+
+  return gifs;
+};
